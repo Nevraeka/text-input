4d86d6470a6904d9b4b4622ec5fc1b54340eafc5
Erik Isaksen <erikisaksen@carfax.com>
2018-05-01 15:10:17 -0400

    [v0.1.0] Removes promises

--- text-input.js
+++ text-input.js
@@ -1,21 +1,18 @@
 (function (document, window) {
 
   const customElementsExist = !!window.customElements;
-  new Promise().then(()=> {
     if (!document.getElementById('#wc-polyfill') && !customElementsExist) {
       const script = document.createElement('script');
       script.id = 'wc-polyfill';
       script.src = 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.2.0/webcomponents-sd-ce.js';
       document.head.appendChild(script);
     }
-  }).then(()=> {
 
   if (customElementsExist && !!!window.customElements.get('img-icon')) {
     const scrpt = document.createElement('script');
     scrpt.src = 'https://cdn.rawgit.com/Nevraeka/img-icon/master/img-icon.js'
     document.head.appendChild(scrpt);
   }
-}).then(()=> {
 
   function loadComponent() {
     if (!customElementsExist) {
@@ -172,5 +169,4 @@
     }
   }
   loadComponent();
-});
 })(document, window);
